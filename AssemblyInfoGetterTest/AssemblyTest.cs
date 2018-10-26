6d322b3259f1a5b156a1364de495e7f6e4f81fd0
Ingvar <shymanski.ihar@gmail.com>
2018-10-26 17:41:51 +0300

    Basic tests

---
+++ AssemblyInfoGetterTest/AssemblyTest.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using AssemblyInfoGetterLib;
+
+namespace AssemblyInfoGetterTest
+{
+    [TestClass]
+    public class AssemblyTest
+    {
+        private Node Setup()
+        {
+            IInfoGetter getter = new AssemblyInfoGetter();
+            return getter.GetFileInfo("TestDll.dll");
+        }
+
+        [TestMethod]
+        public void LoadingAssemblyTest()
+        {
+            Node actual = Setup();
+            Assert.IsNotNull(actual);
+        }
+
+        [TestMethod]
+        public void AssemblyNameTest()
+        {
+            Node actual = Setup();
+            Assert.IsTrue(actual.Content.StartsWith("TestDll"));
+        }
+
+        [TestMethod]
+        public void CompositeNamespaceTest()
+        {
+            Node actual = Setup();
+            Assert.AreEqual("TestDll", actual.Nodes[0].Content);
+            Assert.AreEqual("TestClass1", actual.Nodes[0].Nodes[0].Content);
+        }
+
+        [TestMethod]
+        public void TypeNameTest()
+        {
+            Node actual = Setup();
+            Assert.AreEqual("Class1", actual.Nodes[0].Nodes[0].Nodes[0].Content);
+        }
+
+        [TestMethod]
+        public void ExtensionMethodsIdentifyTest()
+        {
+            Node actual = Setup();
+            Assert.AreEqual("Extension methods", actual.Nodes[0].Nodes[0].Nodes[0].Nodes[3].Content);
+        }
+    }
+}
--- TestDll/Class1.cs
+++ TestDll/Class1.cs
@@ -6,20 +6,19 @@ using System.Threading.Tasks;
 
 namespace TestDll
 {
-    public class Class1
+    namespace TestClass1
     {
-        public void Method1()
+        public class Class1
         {
+            public int field;
+            public int Property { get; set; }
+            public void Method1() { }
+        }
 
+        public static class Class1Ext
+        {
+            public static void Method2(this Class1 obj) { }
+            public static void Method3(this Class1 obj, string str) { }
         }
     }
-
-    public static class Class1Ext
-    {
-        public static void Method2(this Class1 obj)
-        {
-
-        }
-    }
-
 }
